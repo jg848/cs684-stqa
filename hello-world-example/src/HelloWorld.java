28de1302ca59229d1e525f26a2865f787e68fa43
jg848 <jg848@njit.edu>
2022-01-25 03:38:15 -0500

    hello world sample

---
+++ hello-world-example/src/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * Sample Hello World Program.
+ * 
+ * @author Jaswin
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args input to main method.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
