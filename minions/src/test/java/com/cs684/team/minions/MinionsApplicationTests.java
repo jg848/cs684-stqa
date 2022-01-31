119f7028c9d077b0285353fa093cc21bbbafcb47
jg848 <jg848@njit.edu>
2022-01-31 16:24:33 -0500

    created spring boot starter project

---
+++ minions/src/test/java/com/cs684/team/minions/MinionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs684.team.minions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
