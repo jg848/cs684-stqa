88e099dcf328584ae1b22373beab68797e0bded4
jg848 <jg848@njit.edu>
2022-02-22 18:30:05 -0500

    HTML/CSS

--- project/src/main/java/com/cs684/project/user/UserController.java
+++ project/src/main/java/com/cs684/project/user/UserController.java
@@ -14,40 +14,63 @@ public class UserController {
 
 	@PostMapping("/users/signup")
 	public ResponseEntity<String> signUp(@Valid @RequestParam String username, @Valid @RequestParam String password) {
-		User newUser = new User(username, password);
-		List<User> users = userRepository.findAll();
-		for (User user : users) {
-			if (user.equals(newUser)) {
-				return new ResponseEntity<String>("Access denial due to duplicate username.", HttpStatus.UNAUTHORIZED);
+		try {
+			if (username.isBlank() || username.isEmpty() || password.isBlank() || password.isEmpty()) {
+				throw new IllegalArgumentException();
 			}
+			User newUser = new User(username, password);
+			List<User> users = userRepository.findAll();
+			for (User user : users) {
+				if (user.equals(newUser)) {
+					return new ResponseEntity<String>("Access denial due to duplicate username.",
+							HttpStatus.UNAUTHORIZED);
+				}
+			}
+			userRepository.save(newUser);
+			return new ResponseEntity<String>("User is signed up and authorized.", HttpStatus.OK);
+		} catch (IllegalArgumentException e) {
+			return new ResponseEntity<String>("Bad Request.", HttpStatus.BAD_REQUEST);
 		}
-		userRepository.save(newUser);
-		return new ResponseEntity<String>("User is signed up and authorized.", HttpStatus.OK);
 	}
 
 	@PostMapping("/users/signin")
 	public ResponseEntity<String> signIn(@Valid @RequestParam String username, @Valid @RequestParam String password) {
-		User user = new User(username, password);
-		List<User> users = userRepository.findAll();
-		for (User other : users) {
-			if (other.equals(user)) {
-				userRepository.save(user);
-				return new ResponseEntity<String>("User is authorized.", HttpStatus.OK);
+		try {
+			if (username.isBlank() || username.isEmpty() || password.isBlank() || password.isEmpty()) {
+				throw new IllegalArgumentException();
 			}
+			User user = new User(username, password);
+			List<User> users = userRepository.findAll();
+			for (User other : users) {
+				if (other.equals(user)) {
+					userRepository.save(user);
+					return new ResponseEntity<String>("User is authorized.", HttpStatus.OK);
+				}
+			}
+			return new ResponseEntity<String>("User is not authorized.", HttpStatus.UNAUTHORIZED);
+		} catch (IllegalArgumentException e) {
+			return new ResponseEntity<String>("Bad Request.", HttpStatus.BAD_REQUEST);
 		}
-		return new ResponseEntity<String>("User is not authorized.", HttpStatus.UNAUTHORIZED);
 	}
 
 	@PostMapping("/users/signout")
 	public ResponseEntity<String> signOut(@Valid @RequestParam String username, @Valid @RequestParam String password) {
-		User user = new User(username, password);
-		List<User> users = userRepository.findAll();
-		for (User other : users) {
-			if (other.equals(user)) {
-				userRepository.save(user);
-				return new ResponseEntity<String>("User is signed out.", HttpStatus.OK);
+		try {
+			if (username.isBlank() || username.isEmpty() || password.isBlank() || password.isEmpty()) {
+				throw new IllegalArgumentException();
 			}
+			User user = new User(username, password);
+			List<User> users = userRepository.findAll();
+			for (User other : users) {
+				if (other.equals(user)) {
+					userRepository.save(user);
+					return new ResponseEntity<String>("User is signed out.", HttpStatus.OK);
+				}
+			}
+			return new ResponseEntity<String>("User is not signed out.", HttpStatus.NOT_FOUND);
+
+		} catch (IllegalArgumentException e) {
+			return new ResponseEntity<String>("Bad Request.", HttpStatus.BAD_REQUEST);
 		}
-		return new ResponseEntity<String>("User is not signed out.", HttpStatus.NOT_FOUND);
 	}
 }
